3bc16930e3a0c474aebb824026e7e73de4ff1b61
hyunjun9788 <namhyunjun9788@gmail.com>
2023-07-06 16:21:15 +0900

    feat: AttendaceBook.js 생성 및 완료

---
+++ src/AttendanceBook.js
@@ -0,0 +1,26 @@
+const students = [
+    {
+        name:"Inje",
+    },
+    {
+        name:"Steve",
+    },
+    {
+        name:"Bill",
+    },
+    {
+        name:"Jeff"
+    },
+];
+
+function AttendanceBook(props){
+    return (
+        <ul>
+            {students.map((student)=>{
+                return <li>{student.name}</li>
+            })}
+        </ul>
+    )
+}
+
+export default AttendanceBook
